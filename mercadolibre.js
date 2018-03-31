8cdc4b2e32900fba09912e099c567866cb9d1e72
Eibriel <eibriel@yahoo.com.ar>
2018-03-31 00:39:39 -0300

    Adding missing file

---
+++ mercadolibre.js
@@ -0,0 +1,32 @@
+/**
+ * JavaScript Get URL Parameter
+ *
+ * @param String prop The specific URL parameter you want to retreive the value for
+ * @return String|Object If prop is provided a string value is returned, otherwise an object of all properties is returned
+ */
+function getUrlParams( prop ) {
+    var params = {};
+    var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '#' ) + 1 ) );
+    var definitions = search.split( '&' );
+
+    definitions.forEach( function( val, key ) {
+        var parts = val.split( '=', 2 );
+        params[ parts[ 0 ] ] = parts[ 1 ];
+    } );
+
+    return ( prop && prop in params ) ? params[ prop ] : params;
+}
+
+access_token = getUrlParams("access_token");
+getUrlParams("expires_in");
+getUrlParams("domains");
+console.log();
+
+$.ajax({
+  method: "GET",
+  url: "https://api.mercadolibre.com/users/me",
+  data: { "access_token": access_token}
+}).done(function( msg ) {
+    console.log( msg );
+    $("#nickname").html(msg['nickname']);
+});
